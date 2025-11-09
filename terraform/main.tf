523374f1973021558208bc3b5484983582bea900
binuriweerasinghe <binuriweerasinghe123@gmail.com>
2025-11-09 22:31:32 +0530

    Update Terraform and Ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,14 +16,14 @@ resource "docker_network" "devops_network" {
 
 # Pull existing server image from Docker Hub
 resource "docker_image" "server_image" {
-  name = "binuriweerasinghe/devops_project-server:latest"
+  name         = "binuriweerasinghe/devops_project-server:latest"
   keep_locally = true
 }
 
 # Server container
 resource "docker_container" "server" {
   name  = "devops_project-server"
-  image = docker_image.server_image.latest
+  image = docker_image.server_image.name
 
   networks_advanced {
     name = docker_network.devops_network.name
@@ -38,18 +38,23 @@ resource "docker_container" "server" {
     "MONGODB_URI=mongodb://mongo:27017/myDatabase",
     "HOST=0.0.0.0"
   ]
+
+  depends_on = [
+    docker_image.server_image,
+    docker_network.devops_network
+  ]
 }
 
 # Pull existing client image from Docker Hub
 resource "docker_image" "client_image" {
-  name = "binuriweerasinghe/devops_project-client:latest"
+  name         = "binuriweerasinghe/devops_project-client:latest"
   keep_locally = true
 }
 
 # Client container
 resource "docker_container" "client" {
   name  = "devops_project-client"
-  image = docker_image.client_image.latest
+  image = docker_image.client_image.name
 
   networks_advanced {
     name = docker_network.devops_network.name
@@ -59,7 +64,41 @@ resource "docker_container" "client" {
     internal = 3000
     external = 3000
   }
+
+  depends_on = [
+    docker_image.client_image,
+    docker_network.devops_network
+  ]
 }
+
+# Optional: Mongo container (if you want Terraform to manage it too)
+resource "docker_container" "mongo" {
+  name  = "mongo"
+  image = "mongo:6.0"
+
+  networks_advanced {
+    name = docker_network.devops_network.name
+  }
+
+  ports {
+    internal = 27017
+    external = 27017
+  }
+
+  volumes {
+    host_path      = "${path.module}/mongo-data"
+    container_path = "/data/db"
+  }
+
+  env = [
+    "MONGO_INITDB_DATABASE=myDatabase"
+  ]
+
+  depends_on = [
+    docker_network.devops_network
+  ]
+}
+
 
 
 
